155598f5e1bab0736cc9f973d4e3e09a9d65d9da
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-11-26 12:50:59 -0800

    Initial Commit

---
+++ Working/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Working
+//
+//  Created by Alphonso Sensley II on 11/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
